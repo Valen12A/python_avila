0c860c7212da43ca34a68d384a87ffbc5f19a7a2
valentina <valeavila717@gmail.com>
2023-05-03 22:17:15 -0500

    intoduccion a listas

---
+++ listas/ejercicio1.py
@@ -0,0 +1,32 @@
+#suma, promedio, minimo, maximo, moda, media, desviasion estandar.
+import random
+lista = []
+suma = 0
+menor= 0
+mayor = 0
+prom = 0
+
+tam = random.randint(10, 20)
+print (tam)
+for i in range (tam):
+    num=random.randrange(100)
+    lista.append(num)  
+print (lista)
+
+for  i in lista:
+    suma = + suma + i
+print ("la suma es: ", suma)
+
+
+for i in range (len(lista)):
+    suma = lista + lista + i
+    prom = suma// (len (lista))
+print ("el promedio es: ", prom)
+
+for i in lista:
+    if i > mayor:
+        mayor = i
+    if i < menor:
+        menor = i
+print ("el numero mayor es: ", mayor)
+print ("el numero menor es:", menor)
